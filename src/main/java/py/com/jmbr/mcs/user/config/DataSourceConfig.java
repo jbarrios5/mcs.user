5705b6a465e6a7697cd85c5a3ab02e9597de9ab7
juan <marcecinco@fpuna.edu.py>
2023-11-23 18:28:36 -0300

    SE agrega datasource

--- src/main/java/py/com/jmbr/mcs/user/config/DataSourceConfig.java
+++
@@ -1,9 +0,0 @@
-package py.com.jmbr.mcs.user.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class DataSourceConfig {
-
-
-}
--- src/main/java/py/com/jmbr/mcs/user/dao/UserDAOImpl.java
+++ src/main/java/py/com/jmbr/mcs/user/dao/UserDAOImpl.java
@@ -1,10 +1,18 @@
 package py.com.jmbr.mcs.user.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import py.com.jmbr.java.commons.domain.mcs.user.User;
 import py.com.jmbr.java.commons.domain.mcs.user.UserGetRes;
+
+import javax.sql.DataSource;
+
 @Repository
 public class UserDAOImpl implements  UserDAO {
+
+    @Autowired
+    private JdbcTemplate jdbcPGS;
     @Override
     public UserGetRes getUserByDocument(String document) {
         UserGetRes res = new UserGetRes();
@@ -14,6 +22,7 @@ public class UserDAOImpl implements  UserDAO {
         user.setId(12l);
         res.setUser(user);
         return res;
+
     }
 
     @Override
