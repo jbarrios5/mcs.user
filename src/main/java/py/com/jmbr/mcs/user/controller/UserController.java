e932ff98ba0464c8e66c92d66331680705b7a336
juan <marcecinco@fpuna.edu.py>
2023-12-01 13:23:27 -0300

    Se corrige git ignore

--- src/main/java/py/com/jmbr/mcs/user/controller/UserController.java
+++ src/main/java/py/com/jmbr/mcs/user/controller/UserController.java
@@ -3,13 +3,11 @@ package py.com.jmbr.mcs.user.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import py.com.jmbr.java.commons.beans.mcs.user.UserGetResData;
 import py.com.jmbr.java.commons.beans.mcs.user.UserPostReqData;
 import py.com.jmbr.java.commons.beans.mcs.user.UserPostResData;
 import py.com.jmbr.java.commons.context.SecurityAccess;
-import py.com.jmbr.java.commons.exception.JMBRException;
 import py.com.jmbr.mcs.user.constant.Mark;
 import py.com.jmbr.mcs.user.constant.UserConstant;
 import py.com.jmbr.mcs.user.service.UserService;
@@ -24,9 +22,8 @@ public class UserController {
 
     @GetMapping(value = "/")
     @Operation(summary = "Get user",description = "Get an user by document")
-    @SecurityAccess()
     public UserGetResData getUser(
-            @RequestHeader(value = UserConstant.ACCESS_TOKEN) String token,
+
             @RequestParam(value = "document",required = true) String document)
     {
         return service.getUserByDocument(document);
